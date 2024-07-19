2551bef7bdb4a097970ac64b77de24deac2c6a3d
Baran <Baran.soysuren@gmail.com>
2024-07-19 19:50:43 +0200

    change app to use flask-session instead of default

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,20 @@
 import os
 from flask import Flask
+from flask_session import Session
 
 def create_app(test_config=None):
     # Creates and configures app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY="dev",   # Needs to be changed when deploying
-        DATABASE=os.path.join(app.instance_path, "app.sqlite")
+        DATABASE=os.path.join(app.instance_path, "app.sqlite"),
+        SESSION_PERMANENT=False,
+        SESSION_TYPE="filesystem",
+        SESSION_FILE_DIR=os.path.join(app.instance_path, "flask_session")
     )
+    
+    # Initialize the session with the app
+    Session(app)
 
     if test_config is None:
         # Loads the instance config, if it exists, when not testing
