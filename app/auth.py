99e3b3bd9765f7a72a7889f135ba6b9fed0338aa
Baran <Baran.soysuren@gmail.com>
2024-07-19 17:36:39 +0200

    add function to handle register route

--- app/auth.py
+++ app/auth.py
@@ -5,3 +5,35 @@ from app.db import get_db
 
 bp = Blueprint("auth",__name__, url_prefix="/auth")
 
+
+@bp.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        confirmation = request.form.get("confirmation")
+
+        if not username:
+            error = "Username is required"
+        elif not password:
+            error = "Password is required"
+        elif not confirmation:
+            error = "Confirmation is required"
+
+        if error is None:
+            try:
+                db.execute(
+                    "INSERT INTO user (username, hashed_password) VALUES (? , ?)", username, generate_password_hash(password)
+                )
+            except db.IntegrityError:
+                error = f"User {username} is already registered."
+            else:
+                return redirect(url_for("auth.login")) 
+            
+        
+        flash(error)
+    
+    else:
+        return render_template("auth/register.html")
+
+        
