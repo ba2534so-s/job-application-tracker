66d800dbb21ca647fd8469bcb64b2fd431b84389
Baran <Baran.soysuren@gmail.com>
2024-07-19 18:56:19 +0200

    add login route

--- app/auth.py
+++ app/auth.py
@@ -28,6 +28,7 @@ def register():
                 db.execute(
                     "INSERT INTO user (username, hashed_password) VALUES (? , ?)", username, generate_password_hash(password)
                 )
+                db.commit()
             except db.IntegrityError:
                 error = f"User {username} is already registered."
             else:
@@ -39,4 +40,34 @@ def register():
     else:
         return render_template("auth/register.html")
 
+
+@bp.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.method.get("username")
+        password = request.method.get("password")
+        error = None
+
+        if not username:
+            error = "Username is required"
+        elif not password:
+            error = "Password is required"
+
+        if error is None:
+            db = get_db()
+            user = db.execute("SELECT * FROM user WHERE username = ?", username).fetchone()
+
+        if user is None:
+            error = "Incorrect username"
+        elif not check_password_hash(user["password"], password):
+            error = "Incorrect password"
         
+        if error is None:
+            session.clear()
+            session["user_id"] = user["id"]
+            return redirect(url_for("index"))
+        
+        flash(error)
+
+    else:
+        return render_template("auth/login.html")
