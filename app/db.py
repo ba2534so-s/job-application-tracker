069f1fac50b147d149246b3808416997deb74077
Baran <Baran.soysuren@gmail.com>
2024-07-19 13:20:54 +0200

    add functions that will run SQL commands in schema.sql that creates tables

--- app/db.py
+++ app/db.py
@@ -4,7 +4,6 @@ from flask import current_app, g
 
 # Later try to learn and use SQLAlchemy
 
-
 # Creates (if not already created) and returns a database connection
 def get_db():
     if "db" not in g:
@@ -22,3 +21,18 @@ def close_db(e=None):
 
     if db is not None:
         db.close()
+
+# Gets database connection and executes commands in schema.sql file
+def init_db():
+    db = get_db()
+    
+    with current_app.open_resource("schema.sql") as f:
+        db.executescript(f.read().decode("utf8"))
+
+
+# Defines a command line command "init-db" that calls the init_db function
+@click.command("init-db")
+def init_db_command():
+    # Clears the existing data and creates new tables
+    init.db()
+    click.echo("Database initialized")
