0c7065104dd44b07c7bcb6112038dd6ca6c95183
Baran <Baran.soysuren@gmail.com>
2024-07-19 22:49:30 +0200

    Add after_request function to ensure responses aren't cached

--- app/__init__.py
+++ app/__init__.py
@@ -35,4 +35,12 @@ def create_app(test_config=None):
     from . import auth
     app.register_blueprint(auth.bp)
 
+    # Ensure responses aren't cached
+    @app.after_request
+    def after_request(response):
+        response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+        response.headers["Expires"] = 0
+        response.headers["Pragma"] = "no-cache"
+        return response
+
     return app
