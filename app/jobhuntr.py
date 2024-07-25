81b3c05e603c69e0a9ca0e240b97b7dbf6687935
Baran <Baran.soysuren@gmail.com>
2024-07-25 09:27:55 +0200

    add initial index route

--- app/jobhuntr.py
+++ app/jobhuntr.py
@@ -4,3 +4,8 @@ from app.auth import login_required
 from app.db import get_db
 
 bp = Blueprint("index", __name__)
+
+bp.route("/")
+def index():
+    # Later we will present statistics and some type of dashboard/overview of the users applications
+    return render_template("jobhuntr/index.html")
