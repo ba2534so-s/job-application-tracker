bd30cad10d12c587b36dc8d3714714c4edfe2eab
Baran <Baran.soysuren@gmail.com>
2024-07-25 08:43:15 +0200

    add blueprint

--- app/index.py
+++ app/index.py
@@ -2,3 +2,5 @@ from flask import Blueprint, flash, g, redirect, render_template, request, url_f
 from werkzeug.exceptions import abort
 from app.auth import login_required
 from app.db import get_db
+
+bp = Blueprint("index", __name__)
