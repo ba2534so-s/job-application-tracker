5fe135e6665b967c176a57c10c5820e63ac7c3f2
Baran <Baran.soysuren@gmail.com>
2024-07-31 14:04:08 +0200

    prevent form from submitting immediately and gather form data in FormData object

--- app/static/add_job.js
+++ app/static/add_job.js
@@ -1,3 +1,5 @@
 document.getElementById("add-job-form").addEventListener("submit", function(event) {
-
+    event.preventDefault();
+    const form = this;
+    const formData = new FormData(form); // gather data from form
 });
