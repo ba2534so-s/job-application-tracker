e4edec85f0d1e8ba71734bfbdcee7b2bf6b5b328
Baran <Baran.soysuren@gmail.com>
2024-07-31 14:06:08 +0200

    add eventlistener that waits for DOM content to load before proceeding with adding eventlisteners

--- app/static/add_job.js
+++ app/static/add_job.js
@@ -1,5 +1,7 @@
-document.getElementById("add-job-form").addEventListener("submit", function(event) {
-    event.preventDefault();
-    const form = this;
-    const formData = new FormData(form); // gather data from form
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("add-job-form").addEventListener("submit", function(event) {
+        event.preventDefault();
+        const form = this;
+        const formData = new FormData(form); // gather data from form
+    });
 });
