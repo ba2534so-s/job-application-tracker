361fb44a70f64c4fdc82632315b4aa3dc873a830
Baran <Baran.soysuren@gmail.com>
2024-08-03 09:26:09 +0200

    add create user function

--- helpers/queries.py
+++ helpers/queries.py
@@ -1,1 +1,11 @@
 from app.db import get_db
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+# USERS
+def create_user(username, email, password):
+    db = get_db()
+    db.execute("INSERT INTO users (id, email, hashed_password) VALUES (?, ?, ?)",
+               (username, email, generate_password_hash(password)))
+    db.commit()
+
