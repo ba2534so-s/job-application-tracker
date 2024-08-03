b522808f46c56e242dfdb50faf96c285ff241092
Baran <Baran.soysuren@gmail.com>
2024-08-03 09:51:51 +0200

    impement get_user_by_id

--- app/auth.py
+++ app/auth.py
@@ -2,7 +2,7 @@ from functools import wraps
 from flask import Blueprint, flash, g, redirect, render_template, request, session, url_for
 from werkzeug.security import check_password_hash, generate_password_hash
 from app.db import get_db
-from helpers.queries import create_user, get_user_by_username
+from helpers.queries import create_user, get_user_by_username, get_user_by_id
 
 bp = Blueprint("auth",__name__, url_prefix="/auth")
 
@@ -77,7 +77,7 @@ def load_logged_in_user():
     if user_id is None:
         g.user = None
     else:
-        g.user = get_db().execute("SELECT * FROM users WHERE id = ?", (user_id,)).fetchone()
+        g.user = get_user_by_id(user_id)
 
 
 @bp.route("/logout")
