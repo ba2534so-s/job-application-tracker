095bd8d984e08a20a79f2e1b87f6220e18b1628e
Baran <Baran.soysuren@gmail.com>
2024-08-08 09:46:40 +0200

    remove import of helpers

--- app/forms.py
+++ app/forms.py
@@ -1,12 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, SubmitField, URLField
-from helpers.queries import get_contract_types_tuple
 
 
 class AddForm(FlaskForm):
     company = StringField(label="Company")
     position = StringField(label="Position")
-    contract_type = SelectField("Contract Type", choices=get_contract_types_tuple())
+    contract_type = SelectField("Contract Type", choices=[])
     location = StringField(label="Location")
     url = URLField(label="URL")
     add_button = SubmitField(label="Add Job")
