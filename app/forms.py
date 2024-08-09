749a28ec82f026560f4fd6c89aeddb68e444f80b
Baran <Baran.soysuren@gmail.com>
2024-08-09 14:44:24 +0200

    add validator list to all fields

--- app/forms.py
+++ app/forms.py
@@ -4,16 +4,16 @@ from wtforms.validators import Email, EqualTo, Length
 
 
 class AddForm(FlaskForm):
-    company = StringField(label="Company")
-    position = StringField(label="Job Position")
-    contract_type = SelectField("Contract Type", choices=[])
-    location = StringField(label="Location")
-    url = URLField(label="URL")
-    add_button = SubmitField(label="Add Job")
+    company = StringField(label="Company", validators=[])
+    position = StringField(label="Job Position", validators=[])
+    contract_type = SelectField("Contract Type", choices=[], validators=[])
+    location = StringField(label="Location", validators=[])
+    url = URLField(label="URL", validators=[])
+    add_button = SubmitField(label="Add Job", validators=[])
 
 class RegisterForm(FlaskForm):
-    username = StringField(label="Username", validators=Length(min=3, max=30))
-    email = StringField(label="Email", validators=Email())
-    password = PasswordField(label="Password", validators=Length(min=8))
-    password_confirm = PasswordField(label="Confirm Password", validators=EqualTo("password"))
+    username = StringField(label="Username", validators=[Length(min=3, max=30)])
+    email = StringField(label="Email", validators=[Email()])
+    password = PasswordField(label="Password", validators=[Length(min=8)])
+    password_confirm = PasswordField(label="Confirm Password", validators=[EqualTo("password")])
     submit_button = SubmitField(label="Create User")
