e53681c6d5a6d91a7bf50d2a241a8b3c24d86534
Baran <Baran.soysuren@gmail.com>
2024-08-16 22:43:00 +0200

    remove old add code

--- app/jobhuntr.py
+++ app/jobhuntr.py
@@ -24,48 +24,6 @@ def index():
 @bp.route("/add", methods=["GET", "POST"])
 @login_required
 def add():
-
-    '''
-    if request.method == "POST":
-        company = request.form.get("company")
-        position = request.form.get("position")
-        contract_type = request.form.get("contract-type")
-        location = request.form.get("location")
-        url = request.form.get("url")
-        date_added = datetime.now().strftime("%Y-%m-%d")
-        error = None
-
-        if not company:
-            error = "Company name required"
-        elif not position:
-            error = "Position required"
-        elif not contract_type:
-            error = "Contract type required"
-        elif not location:
-            error = "Location required"
-            
-        
-        if error is None:
-            if check_existing_application(g.user["id"], company, position, contract_type, location, url, date_added):
-                flash("You have already added this job recently")
-                return redirect(url_for("jobhuntr.add"))
-            
-            add_job(g.user["id"], company, position, location, contract_type, url, date_added)
-            flash("Job application added successfully.")
-            return redirect(url_for("index"))
-
-
-
-        # check radio button if user wants to add contact information
-        # If yes, get contact info
-
-
-    else:
-        form = AddForm()
-        form.contract_type.choices = [("", "Select Contract Type")] + get_contract_types_tuple()
-        return render_template("jobhuntr/add.html", form=form)
-    '''
-        
     form = AddForm()
     form.contract_type.choices = [("", "Select Contract Type")] + get_contract_types_tuple()
 
