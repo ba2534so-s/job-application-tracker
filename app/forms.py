a0936f98c9851afa997be331ca130a0752973314
Baran <Baran.soysuren@gmail.com>
2024-08-16 20:41:38 +0200

    add url field

--- app/forms.py
+++ app/forms.py
@@ -17,6 +17,7 @@ class EditForm(FlaskForm):
     position = StringField(label="Job Position", validators=[DataRequired()])
     contract_type = SelectField(label="Contract Type", choices=[], validators=[DataRequired()])
     location = StringField(label="Location", validators=[DataRequired()])
+    url = URLField(label="URL", validators=[Optional(), URL()])
 
 
 class RegisterForm(FlaskForm):
