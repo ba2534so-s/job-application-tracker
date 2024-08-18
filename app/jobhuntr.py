01a9e47cc8a607f85680050a0a290c6bed3472f4
Baran <Baran.soysuren@gmail.com>
2024-08-18 22:19:06 +0200

    add job position data

--- app/jobhuntr.py
+++ app/jobhuntr.py
@@ -55,6 +55,7 @@ def edit(job_id):
 
 
     form.company.data = job["company_name"]
+    form.position.data = job["job_position"]
 
     return render_template("jobhuntr/edit.html", form=form, job=job)
 
