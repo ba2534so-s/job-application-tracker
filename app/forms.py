6ee61dda58e54972c97a5e64fc1097287f26ddc1
Baran <Baran.soysuren@gmail.com>
2024-10-02 19:40:59 +0200

    change design of button

--- app/forms.py
+++ app/forms.py
@@ -5,7 +5,7 @@ from helpers.queries import get_user_by_username, get_user_by_email
 
 
 class ContactForm(FlaskForm):
-    name = StringField(label="Contact Name", validators=[DataRequired()])
+    name = StringField(label="Contact Name*", validators=[DataRequired()])
     email = StringField(label="Email", validators=[Optional(), Email()])
     phone = StringField(label="Phone Number", validators=[Optional()])
 class AddForm(FlaskForm):
