278df271dfb62bc806e6e5756b0bcda327ed8630
Baran <Baran.soysuren@gmail.com>
2024-10-02 20:35:20 +0200

    initially set contact_id as None

--- helpers/queries.py
+++ helpers/queries.py
@@ -80,6 +80,7 @@ def add_job(user_id, company, position, location, contract_type, url, contact_in
     date_added = datetime.now().strftime("%Y-%m-%d")
     db = get_db()
     
+    contact_id = None
     if contact_info is not None:
         contact_id = add_contact(contact_info) if contact_info else None
     
