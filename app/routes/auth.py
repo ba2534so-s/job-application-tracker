db68fa8302ddeac4f0b0152f5a2ba3e1fd838b69
Baran <Baran.soysuren@gmail.com>
2024-10-23 23:08:07 +0200

    remove decorator function from routes file

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -58,11 +58,3 @@ def logout():
     flash("You have been logged out", category="info")
     return redirect(url_for("index"))
 
-
-def login_required(view):
-    @wraps(view)
-    def wrapped_view(**kwargs):
-        if g.user is None:
-            return redirect(url_for("auth.login"))
-        return view(**kwargs)
-    return wrapped_view
