e68e8de6330891cd1073b73cc8eef052783a0e21
Baran <Baran.soysuren@gmail.com>
2024-10-23 20:44:02 +0200

    clear the app/routes/__init__.py file

--- app/routes/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-
-from .auth import bp as auth_bp
-from .jobhuntr import bp as jobhuntr_bp
-
-def init_app(app):
-    app.register_blueprint(auth_bp)
-    app.register_blueprint(jobhuntr_bp)
