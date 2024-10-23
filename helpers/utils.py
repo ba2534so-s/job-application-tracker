5f1ce0f50438a9c01c97bb0915c399db888baba3
Baran <Baran.soysuren@gmail.com>
2024-10-23 23:24:08 +0200

    create populate_edit_form function signature

--- helpers/utils.py
+++ helpers/utils.py
@@ -8,3 +8,7 @@ def login_required(view):
             return redirect(url_for("auth.login"))
         return view(**kwargs)
     return wrapped_view
+
+
+
+def populate_edit_form(form, job, contact):
